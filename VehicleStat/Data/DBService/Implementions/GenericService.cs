4b5d9a36a63b8073b5fdb88f6a78370577c50abd
Prabhath <prabhathg@gmail.com>
2015-11-06 23:06:35 +0530

    DB Service Init

---
+++ VehicleStat/Data/DBService/Implementions/GenericService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VehicleStat.Data.DBModel;
+
+namespace Data.DBService.Implementions
+{
+    class GenericService<T>
+    {
+        protected static vehicle_dbEntities db = new vehicle_dbEntities();
+
+        public static void Insert(T entity)
+        {
+            // entity.
+        }
+
+        public static void Update(T entity)
+        {
+            // entity.
+        }
+
+        public static void Delete(T entity)
+        {
+            // entity.
+        }
+    }
+}
---
+++ VehicleStat/Data/DBService/Implementions/VehicleService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data.DBService.Implementions;
+using VehicleStat.Data.DBModel;
+using System.Windows;
+using Util.GUI;
+
+namespace DBService.Implementions
+{
+    class VehicleService:GenericService<vehicle>
+    {
+        internal static vehicle GetVehicleByRegistrationNumber(string p)
+        {
+            try
+            {
+                vehicle _vehicle= db.vehicles.Where(v => v.registration_no.Trim() == p.Trim()).SingleOrDefault();
+                return _vehicle;
+            }
+            catch(Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+                return null;
+            }
+        }
+
+        internal static Util.GUI.PagingCollection<vehicle> GetSearchedVehicleListByPage(int page,string _searchText)
+        {
+            PagingCollection<vehicle> pager = new PagingCollection<vehicle>();
+            int pagesize = pager.PageSize;
+            int offset = pager.PageSize * (page - 1);
+
+            List<vehicle> _vehicle;
+
+            _vehicle = db.vehicles.Where(v => v.registration_no.Trim() == _searchText.Trim()).ToList();
+
+            pager.Collection = _vehicle.Skip(offset).Take(pagesize).ToList();
+            pager.TotalCount = _vehicle.Count();
+            pager.CurrentPage = page;
+
+            return pager;
+        }
+    }
+}
