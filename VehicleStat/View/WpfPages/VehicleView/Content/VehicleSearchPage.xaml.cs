68d8f4f1dba1684d6c60f12dc1319f0d7c5938dc
Prabhath <prabhathg@gmail.com>
2015-11-06 23:03:31 +0530

    Dashboard and Search page init

---
+++ VehicleStat/View/WpfPages/VehicleView/Content/VehicleSearchPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System;
+using Util.GUI;
+using VehicleStat.Data.DBModel;
+using DBService.Implementions;
+
+
+namespace View.WpfPages.VehicleView.Content
+{
+    /// <summary>
+    /// Interaction logic for Employee QuickSearchPage.xaml
+    /// </summary>
+    public partial class VehicleSearchPage : Page, INotifyPropertyChanged
+    {
+        private static VehicleSearchPage _instance;
+        private string _searchText;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<vehicle> VehicleList;
+
+        public List<PageData> PagingList { get; set; }
+        private PagingCollection<vehicle> _PagingCollection { get; set; }
+
+        private VehicleSearchPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public static VehicleSearchPage Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new VehicleSearchPage();
+                }
+                return _instance;
+            }
+        }
+
+        public string SearchText
+        {
+            get
+            {
+                return _searchText;
+            }
+            set
+            {
+                _searchText = value;
+                OnPropertyChanged("SearchText");
+            }
+        }
+
+        private void RefreshVehicleListByPage(int page)
+        {
+            _PagingCollection = VehicleService.GetSearchedVehicleListByPage(page,_searchText);
+
+            VehicleList = _PagingCollection.Collection;
+            PagingList = _PagingCollection.PagesList;
+
+            SearchedVehicleListView.ItemsSource = VehicleList;
+            SearchedVehicleListView.Items.Refresh();
+
+            PagingListView.ItemsSource = PagingList;
+            PagingListView.Items.Refresh();
+        }
+
+        private void QuickSearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshVehicleListByPage(1);
+        }
+
+
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ VehicleStat/View/WpfPages/VehicleView/VehicleViewDashBoard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Net;
+using System.Windows.Input;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.ComponentModel;
+using View.WpfPages.VehicleView.Content;
+
+namespace View.WpfPages.VehicleView
+{
+    /// <summary>
+    /// Interaction logic for EmployeePage.xaml
+    /// </summary>
+    public partial class VehicleViewDashBoard : Page
+    {
+        private static VehicleViewDashBoard _instance;
+
+
+        public VehicleViewDashBoard()
+        {
+            InitializeComponent();
+            ContentFrame.Content = VehicleSearchPage.Instance;
+        }
+
+        public static VehicleViewDashBoard Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new VehicleViewDashBoard();
+                }
+                return _instance;
+            }
+        }
+    }
+       
+}
